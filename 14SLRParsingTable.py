f36a4b37688dad2e9739425a6ac3554bcff9a240
VaishnavSherla <vaishnavsherla@gmail.com>
2024-05-30 05:01:04 +0530

    Updated SLR

--- 14SLRParsingTable.py
+++ 14SLRParsingTable.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from tabulate import tabulate
 
 class SLRParser:
     def __init__(self, grammar, terminals):
@@ -19,7 +20,7 @@ class SLRParser:
             n = len(closure)
             temp_cl = closure.copy()
             for lhs, rhs in closure:
-                if rhs[-1] == '.':
+                if rhs[-1] == '.':  # If dot is already at the end, skip
                     continue
                 dot_index = rhs.index('.')
                 B = rhs[dot_index+1]
@@ -32,45 +33,34 @@ class SLRParser:
             if len(closure) == n:
                 return closure
     
-    def findGOTO(self, state_num):
-        closure = self.states[state_num]
-        new = set()
-        gotos = defaultdict(list)
-        for lhs, rhs in closure:
-            if rhs[-1] == '.':
-                continue
-            dot_index = rhs.index('.')
-            charNextToDot = rhs[dot_index+1]
-            rhs = rhs[:dot_index] + charNextToDot + '.' + rhs[dot_index+2:]
-            new_cl = [(lhs, rhs)]
-            new_cl = self.findClosure(new_cl)
-            gotos[charNextToDot].extend(new_cl)
+    def GOTO(self, states):
+        while states:
+            state_num, closure = states.pop(0)
+            self.states[state_num] = closure
+            gotos = defaultdict(list)
+            for lhs, rhs in closure:
+                if rhs[-1] == '.':
+                    continue
+                dot_index = rhs.index('.')
+                charNextToDot = rhs[dot_index+1]
+                rhs = rhs[:dot_index] + charNextToDot + '.' + rhs[dot_index+2:]
+                new_cl = [(lhs, rhs)]
+                new_cl = self.findClosure(new_cl)
+                gotos[charNextToDot].extend(new_cl)
 
-        for lhs, new_cl in gotos.items():
-            for num, cl in self.states.items():
-                if new_cl == cl:
-                    self.statemap[(state_num, lhs)] = num
-                    break
-            else:
-                self.count += 1
-                self.states[self.count] = new_cl
-                self.statemap[(state_num, lhs)] = self.count
-                new.add(self.count)
-        return new
-    
-    def GOTO(self, num):
-        new = self.findGOTO(num)
-        while True:
-            n = len(new)
-            new1 = new.copy()
-            for i in new:
-                new1 |= self.findGOTO(i)
-            new = new1
-            if len(new) == n:
-                break
+            for lhs, new_cl in gotos.items():
+                for num, cl in self.states.items():
+                    if new_cl == cl:
+                        self.statemap[(state_num, lhs)] = num
+                        break
+                else:
+                    self.count += 1
+                    self.states[self.count] = new_cl
+                    self.statemap[(state_num, lhs)] = self.count
+                    states.append((self.count, new_cl))
     
     def buildParsingTable(self):
-        self.GOTO(0)
+        self.GOTO([(0, self.findClosure(self.closure))])
         cols = self.terminals + ['$'] + self.NT
         Table = [['']*len(cols) for _ in range(self.count+1)]
         for map, s in self.statemap.items():
@@ -80,27 +70,22 @@ class SLRParser:
                 Table[num][index] = f'{s}'
             else:
                 Table[num][index] = f'S{s}'
+        
+        print("Closure States: ", self.states)
         for num, cl in self.states.items():
             for lhs, rhs in cl:
                 if rhs[-1] == '.':
                     if rhs[0] == self.start_symbol and lhs == self.new_start_symbol:
                         index = cols.index('$')
-                        Table[num][index] = 'Accept'
+                        Table[num][index] = 'Accept'  # Accept action
                     else:
                         prod_num = self.rules.index((lhs, rhs[:-1]))
                         follow = self.computeFollow(lhs)
                         for i in follow:
                             index = cols.index(i)
-                            Table[num][index] = f'R{prod_num+1}'
-        print("\nSLR(1) parsing table:\n")
-        frmt = "{:>8}" * len(cols)
-        print("   ", frmt.format(*cols), "\n")
-        ptr = 0
-        j = 0
-        for y in Table:
-            print(f"{{:>3}} {frmt.format(*y)}".format('I' + str(j)))
-            j += 1
-
+                            Table[num][index] = f'R{prod_num+1}'  # Reduce action
+        print(tabulate(Table, headers=cols, showindex="always", tablefmt="grid"))
+    
     def computeFirst(self, variable):
         first = set()
         if not variable.isupper():
@@ -116,7 +101,6 @@ class SLRParser:
         follow = set()
         if variable == self.start_symbol:
             follow.add('$')
-
         for lhs, rhs in self.rules:
             for index, nonTerminal in enumerate(rhs):
                 if nonTerminal == variable:
@@ -141,7 +125,5 @@ class SLRParser:
 CFG = {'E': ['E+T', 'T'],
        'T': ['T*F', 'F'],
        'F': ['(E)', 'i']}
-
 T = ['+', '*', '(', ')', 'i']
-
 SLRParser(CFG, T)
